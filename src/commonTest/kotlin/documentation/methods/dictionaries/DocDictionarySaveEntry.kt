a3af62d2531759b0de06aa7957d08a0583a80830
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-02-24 16:22:38 +0100

    chore(dictionaries): add doc save entry snippets

--- src/commonTest/kotlin/documentation/methods/dictionaries/DocDictionarySaveEntry.kt
+++ src/commonTest/kotlin/documentation/methods/dictionaries/DocDictionarySaveEntry.kt
@@ -1,7 +1,60 @@
 package documentation.methods.dictionaries
 
+import com.algolia.search.endpoint.extension.saveCompoundEntries
+import com.algolia.search.endpoint.extension.savePluralEntries
+import com.algolia.search.endpoint.extension.saveStopwordEntries
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.dictionary.Dictionary
+import com.algolia.search.model.dictionary.DictionaryEntry
+import com.algolia.search.model.search.Language
+import documentation.client
 import kotlin.test.Ignore
+import kotlin.test.Test
+import runBlocking
 
 @Ignore
 class DocDictionarySaveEntry {
+
+    @Test
+    fun snippetStopword() {
+        runBlocking {
+            val entry = DictionaryEntry.Stopword(
+                objectID = ObjectID("MyObjectID"),
+                language = Language.English,
+                word = "upper"
+            )
+            client.saveDictionaryEntries(Dictionary.Stopwords, listOf(entry))
+            // or using extension function:
+            client.saveStopwordEntries(listOf(entry))
+        }
+    }
+
+    @Test
+    fun snippetPlural() {
+        runBlocking {
+            val entry = DictionaryEntry.Plural(
+                objectID = ObjectID("MyObjectID"),
+                language = Language.French,
+                words = listOf("cheval", "chevaux")
+            )
+            client.saveDictionaryEntries(Dictionary.Plurals, listOf(entry))
+            // or using extension function:
+            client.savePluralEntries(listOf(entry))
+        }
+    }
+
+    @Test
+    fun snippetCompound() {
+        runBlocking {
+            val entry = DictionaryEntry.Compound(
+                objectID = ObjectID("MyObjectID"),
+                language = Language.Dutch,
+                word = "kopfschmerztablette",
+                decomposition = listOf("kopf", "schmerz", "tablette")
+            )
+            client.saveDictionaryEntries(Dictionary.Compounds, listOf(entry))
+            // or using extension function:
+            client.saveCompoundEntries(listOf(entry))
+        }
+    }
 }
