1319d7f3a34de51a762402f20764782cfabd669a
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-04-01 16:09:05 +0200

    wip: target kotlin/js

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.5.32"
+        val kotlinVersion = "1.5.31"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
@@ -16,8 +16,4 @@ subprojects {
     repositories {
         mavenCentral()
     }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.buildDir)
 }
